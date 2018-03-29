bcdff809c0b29b6aa267e800b92c767061fa5d8e
David Teitelbaum <david.teitelbaum@wework.com>
2018-03-29 18:04:26 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,3 +4,25 @@ var chocolateBars = [
   'kitkat',
   'skittles'
 ]
+
+function addElementToBeginningOfArray(array, element) {
+  return ['Pie', ...chocolateBars]
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  chocolateBars.unshift('Pie')
+  return chocolateBars
+}
+
+function addElementToEndOfArray(array, element) {
+  return [...chocolateBars, 'Pie']
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  chocolateBars.push('Pie')
+  return array
+}
+
+function accessElementInArray(array, index) {
+  console.log(chocolateBars[2]);
+}
